0b79d962d1d0dd7532eee75a5dcbe9c8d018b4e9
shiva maddyelamid <shivareddyac820@gmail.com>
2025-12-10 14:13:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-import subprocess
-import sys
-
-subprocess.run([sys.executable, "-m", "pip", "install", "seaborn"])
-import seaborn as sns
-print("Seaborn installed successfully:", sns.__version__)
-
-
 import streamlit as st
 import pandas as pd
 import sqlite3
 from datetime import date
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # --------------------------------------------
